6fd4f05aaa79ee28621b9592a547f9aab4a176a5
chase-stevens <chasestevenspersonal@gmail.com>
2019-01-31 19:21:35 -0700

    bump

--- app/models/article.rb
+++ app/models/article.rb
@@ -1,8 +1,9 @@
 class Article < ApplicationRecord
+  belongs_to :author
   validates :title, presence: true, length: { maximum: 150 }
   validates :text, presence: true
 
   def get_excerpt
-    text.truncate(150) # 30 is char limit
+    text.truncate(250)
   end
 end
