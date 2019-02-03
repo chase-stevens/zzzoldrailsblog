dc3320f48b95567ba129d388fb87afa1701198f2
chase-stevens <chasestevenspersonal@gmail.com>
2019-02-03 14:03:07 -0700

    fixed bug where edit/update created a new article instead of updating

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -11,8 +11,13 @@ class ArticlesController < ApplicationController
     @article = Article.new
   end
 
+  def edit
+    @article = Article.find(params[:id])
+  end
+
   def create
     @article = Article.new(article_params)
+
     if @article.save
       redirect_to @article
     else
@@ -21,13 +26,16 @@ class ArticlesController < ApplicationController
     end
   end
 
-  def edit
-    @article = Article.find(params[:id])
-  end
+
 
   def update
-    @article.save
-    redirect_to @article
+    @article = Article.find(params[:id])
+
+    if @article.update(article_params)
+      redirect_to @article
+    else
+      render 'edit'
+    end
   end
 
   def destroy
